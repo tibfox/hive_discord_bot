96018eb718af4757f220df9c24f835c8136c0955
tibfox <tibfox@gmail.com>
2025-04-16 12:57:13 +0000

    added git version

--- bottemplate/commands/version.go
+++ bottemplate/commands/version.go
@@ -1,7 +1,10 @@
 package commands
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/disgoorg/disgo/discord"
 	"github.com/disgoorg/disgo/handler"
@@ -16,8 +19,113 @@ var version = discord.SlashCommandCreate{
 
 func VersionHandler(b *bottemplate.Bot) handler.CommandHandler {
 	return func(e *handler.CommandEvent) error {
+
+		owner := "tibfox"
+		repo := "hive_discord_bot"
+		url := fmt.Sprintf("https://api.github.com/repos/%s/%s/commits", owner, repo)
+
+		resp, err := http.Get(url)
+		if err != nil {
+			return e.CreateMessage(discord.MessageCreate{
+				Content: noCommitFound(),
+			})
+		}
+		defer resp.Body.Close()
+
+		var commits []Commit
+		if err := json.NewDecoder(resp.Body).Decode(&commits); err != nil {
+			return e.CreateMessage(discord.MessageCreate{
+				Content: noCommitFound(),
+			})
+		}
+
+		if len(commits) == 0 {
+			fmt.Println("No commits found.")
+			return e.CreateMessage(discord.MessageCreate{
+				Content: noCommitFound(),
+			})
+		}
+
+		latest := commits[0]
+		fmt.Printf("Latest Commit:\n")
+		fmt.Printf("SHA: %s\n", latest.SHA)
+		fmt.Printf("Message: %s\n", latest.Commit.Message)
+		fmt.Printf("Date: %s\n", latest.Commit.Author.Date.Format(time.RFC1123))
+
+		// Construct the embed message
+		embed := discord.Embed{
+			Title:       "Version info",
+			Description: fmt.Sprintf("Details about github.com/%s/%s", owner, repo),
+			Color:       0x00ffcc, // Optional: Embed sidebar color
+			Fields: []discord.EmbedField{
+				{
+					Name:   "SHA",
+					Value:  latest.SHA,
+					Inline: BoolPtr(true),
+				},
+				{
+					Name:   "Message",
+					Value:  latest.Commit.Message,
+					Inline: BoolPtr(true),
+				},
+				{
+					Name:   "Date",
+					Value:  latest.Commit.Author.Date.Format(time.RFC1123),
+					Inline: BoolPtr(true),
+				},
+			},
+		}
+
 		return e.CreateMessage(discord.MessageCreate{
-			Content: fmt.Sprintf("Version: %s\nCommit: %s", b.Version, b.Commit),
+			Embeds: []discord.Embed{embed},
 		})
 	}
 }
+
+func BoolPtr(b bool) *bool {
+	return &b
+}
+
+func noCommitFound() string {
+	return "Commit: no commit found"
+}
+
+type Commit struct {
+	Commit struct {
+		Author struct {
+			Date time.Time `json:"date"`
+		} `json:"author"`
+		Message string `json:"message"`
+	} `json:"commit"`
+	SHA string `json:"sha"`
+}
+
+// func main() {
+// 	owner := "vercel"
+// 	repo := "next.js"
+// 	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/commits", owner, repo)
+
+// 	resp, err := http.Get(url)
+// 	if err != nil {
+// 		fmt.Println("Error fetching data:", err)
+// 		os.Exit(1)
+// 	}
+// 	defer resp.Body.Close()
+
+// 	var commits []Commit
+// 	if err := json.NewDecoder(resp.Body).Decode(&commits); err != nil {
+// 		fmt.Println("Error decoding JSON:", err)
+// 		os.Exit(1)
+// 	}
+
+// 	if len(commits) == 0 {
+// 		fmt.Println("No commits found.")
+// 		return
+// 	}
+
+// 	latest := commits[0]
+// 	fmt.Printf("Latest Commit:\n")
+// 	fmt.Printf("SHA: %s\n", latest.SHA)
+// 	fmt.Printf("Message: %s\n", latest.Commit.Message)
+// 	fmt.Printf("Date: %s\n", latest.Commit.Author.Date.Format(time.RFC1123))
+// }
